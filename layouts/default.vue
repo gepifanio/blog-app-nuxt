b2aa636c5163091b2ef63d94d1ea0cc23d73af2c
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2018-11-18 23:42:42 +1100

    blog app using nuxt js and vue

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav"/>
+    <TheSideNav 
+      :show="displaySideNav" 
+      @close="displaySideNav = false"/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader.vue'
+import TheSideNav from '@/components/Navigation/TheSideNav.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheSideNav
+  },
+  data() {
+    return {
+      displaySideNav: false
+    }
+  }
+}
+</script>
+
+
+<style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
